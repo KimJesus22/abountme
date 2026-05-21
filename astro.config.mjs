552dfc4346123d1cca37edcf0216685a956b991c
KimJesus22 <joseceron685@gmail.com>
2026-05-20 19:02:38 -0600

    Corrige despliegue en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://abountme-beta.vercel.app',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: 'en',
